13504129ffe03bbc12da8afd1c414401a1640058
unknown <kweyz@daum.net>
2022-05-18 23:35:24 +0900

    loginPage: logo image

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -1,10 +1,28 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, Image } from "react-native";
 
 const SignInScreen = () => (
-  <View>
-    <Text>로그인 페이지</Text>
+  <View style={styles.block}>
+    <Image
+      source={require("../assets/logo.png")}
+      style={styles.image}
+      resizeMode="contain"
+    />
+    <Text>여기는 버튼 자리</Text>
   </View>
 );
+
+const styles = StyleSheet.create({
+  block: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-around",
+    backgroundColor: "white",
+  },
+  image: {
+    width: 300,
+    height: 300,
+  },
+});
 
 export default SignInScreen;
