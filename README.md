e5aae9f10795b686d23d27bcf20b57c9f8dbe4b4
unknown <kweyz@daum.net>
2022-05-23 14:23:10 +0900

    Google Login screen

---
+++ README.md
@@ -0,0 +1,7 @@
+### 작업 환경
+
+- 안드로이드 스튜디오
+- AMD Device: Pixel 2 API 32
+- IDE: VScode
+- Platform: Create-React-Native-App (CRNA = Expo&RN)
+- Framework: React-native
--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -1,21 +1,48 @@
 import React from "react";
 import { Text, View, StyleSheet, Image, TouchableOpacity } from "react-native";
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+} from "@react-native-google-signin/google-signin";
 
-const SignInScreen = ({ navigation: { navigate } }) => (
-  <View style={styles.block}>
-    <Image
-      source={require("../assets/logo.png")}
-      style={styles.image}
-      resizeMode="contain"
-    />
-    <TouchableOpacity
-      style={styles.button}
-      onPress={() => navigate("Tabs", { screen: "Home" })}
-    >
-      <Text style={styles.text}>얼른 와</Text>
-    </TouchableOpacity>
-  </View>
-);
+GoogleSignin.configure({
+  webClientId:
+    "1011144899300-lc4rpi1gc0gkpa6kq3b753nd66rmltj3.apps.googleusercontent.com",
+  offlineAccess: true,
+});
+
+const SignInScreen = ({ navigation: { navigate } }) => {
+  signIn = async () => {
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+      this.setState({ userInfo });
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  return (
+    <View style={styles.block}>
+      <Image
+        source={require("../assets/logo.png")}
+        style={styles.image}
+        resizeMode="contain"
+      />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigate("Tabs", { screen: "Home" })}
+      >
+        {/* <Text style={styles.text}>얼른 와</Text> */}
+        <GoogleSigninButton
+          onPress={signIn}
+          size={GoogleSigninButton.Size.Wide}
+          color={GoogleSigninButton.Color.Dark}
+        />
+      </TouchableOpacity>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   block: {
