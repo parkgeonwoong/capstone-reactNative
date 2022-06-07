7cd04abbe05a406ea30d1900162d6e0e09cf71a5
unknown <kweyz@daum.net>
2022-06-07 17:27:32 +0900

    Home: Work remove Alert

--- components/WorkItem.js
+++ components/WorkItem.js
@@ -4,7 +4,7 @@
 */
 
 import React from "react";
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet, Alert } from "react-native";
 import styled from "styled-components/native";
 import { Ionicons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
@@ -38,6 +38,25 @@ const RemovePlace = styled.View`
 
 const WorkItem = ({ id, text, done, onToggle, onRemove }) => {
   const navigation = useNavigation(); // Hook: Screen으로 사용되지 않는 컴포넌트에 navigation 객체 사용
+  const remove = () => {
+    Alert.alert(
+      "삭제",
+      "정말로 삭제하시겠어요?",
+      [
+        { text: "취소", onPress: () => {} },
+        {
+          text: "삭제",
+          onPress: () => {
+            onRemove(id);
+          },
+        },
+      ],
+      {
+        cancelable: true,
+        onDismiss: () => {},
+      }
+    );
+  };
 
   return (
     <WrapperItem>
@@ -56,7 +75,7 @@ const WorkItem = ({ id, text, done, onToggle, onRemove }) => {
       <TextBtn onPress={() => onToggle(id)}>
         <ItemText style={done ? styles.lineThrough : null}>{text}</ItemText>
         {done ? (
-          <TouchableOpacity onPress={() => onRemove(id)}>
+          <TouchableOpacity onPress={remove}>
             <Ionicons name="trash-bin" size={28} color={BLACK} />
           </TouchableOpacity>
         ) : (
