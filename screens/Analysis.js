333d7a0efa2c670248296490395e8bb1320b0cf1
unknown <kweyz@daum.net>
2022-07-08 19:44:49 +0900

    ✨ Feat: 캘린더와 로그 기능, 더미데이터

--- screens/Analysis.js
+++ screens/Analysis.js
@@ -1,4 +1,10 @@
-import React, { useContext, useState } from "react";
+/* 
+@컴포넌트 이름: 분석 페이지
+@관련된 컴포넌트: Tabs
+@구현: 캘린더, 날짜별 로그, 차트
+*/
+
+import React, { useContext, useEffect, useState } from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { BG_COLOR } from "../components/Colors";
 import LogContext from "../contexts/LogContext";
@@ -11,11 +17,12 @@ import {
   AgendaSchedule,
 } from "react-native-calendars";
 import { Card, Paragraph } from "react-native-paper";
+import { addDays, format } from "date-fns";
 
-const timeToString = (time) => {
-  const date = new Date(time);
-  return date.toISOString().split("T")[0];
-};
+// const timeToString = (time) => {
+//   const date = new Date(time);
+//   return date.toISOString().split("T")[0];
+// };
 
 const Analysis = () => {
   const { works } = useContext(LogContext);
@@ -24,6 +31,7 @@ const Analysis = () => {
   const date = new Date();
   const today = date.toISOString().split("T")[0];
 
+  // 각 날짜별 상태값
   const [items, setItems] = useState({
     "2022-07-07": [{ name: "카테고리1", count: "총 시간" }],
     "2022-07-08": [
@@ -31,6 +39,37 @@ const Analysis = () => {
       { name: "카테고리2", count: 5 },
     ],
   });
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      const data = await response.json();
+
+      const mappedData = data.map((post, index) => {
+        const dateFns = addDays(new Date(), index);
+
+        return {
+          ...post,
+          // date: format(dateFns, "yyyy-MM-dd"),
+          id: [{ date: format(dateFns, "yyyy-MM-dd") }],
+        };
+      });
+
+      // const mappedData = data.map((post) => {
+      //   return {
+      //     ...post,
+      //     date: today,
+      //   };
+      // });
+
+      console.log(mappedData[0]);
+
+      // setItems(data.slice(0,5))
+    };
+    getData();
+  }, []);
 
   const renderItem = (item) => {
     return (
@@ -70,6 +109,7 @@ const styles = StyleSheet.create({
   },
   selectItem: {
     alignItems: "center",
+    justifyContent: "flex-start",
     // backgroundColor: "tomato",
   },
 });
