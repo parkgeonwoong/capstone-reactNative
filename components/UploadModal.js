5d4efa0acd168d3c332897fab60d8f2fd1c4c991
unknown <kweyz@daum.net>
2022-07-09 23:28:39 +0900

    🐛 Fix: 입력 시 모달창 숨김 수정

--- components/UploadModal.js
+++ components/UploadModal.js
@@ -21,7 +21,7 @@ const UploadModal = ({ visible, onClose, onInsert }) => {
     onInsert(text);
     setText("");
     Keyboard.dismiss();
-    // onClose();
+    onClose();
   };
   // console.log(text);
 
